628d21abc205655e165f800d7038f8ed836892c0
Super Dev <superdev88@hotmail.com>
2019-06-03 07:56:47 -0400

    add filter

--- portfolio/views.py
+++ portfolio/views.py
@@ -28,13 +28,31 @@ class PortfolioListAPIView(ListCreateAPIView):
         print("user: ", self.request.user)
         account = Account.objects.get(email=self.request.user)
         json_data = open('/home/superdev/Documents/myproject/unwravel/portfolio/static/data/victoria_secret.json')
-        temp_portfolios = json.load(json_data)
-        json_portfolios = json.dumps(temp_portfolios)
-        result = []
-        
+        json_portfolios = json.load(json_data)
+        topsize = account.topsize
+        bottomsize = account.bottomsize
+        brasize = account.brasize
+        pantysize = account.pantysize
+        json_result = []
+        for portfolio in json_portfolios:
+            product_category = portfolio['product_category']
+            available_size = portfolio['available_size']
+            print(product_category)
+            if product_category == "Panties":
+                if pantysize in available_size:
+                    json_result.append(portfolio)
+            if product_category == "Lingerie":
+                # if pantysize in available_size:
+                json_result.append(portfolio)
+            if product_category == "Bras":
+                if brasize in available_size:
+                    json_result.append(portfolio)
+            # if product_category == "Panties":
+            #     if pantysize in available_size:
+            #         json_result.append(portfolio)
 
         return Response(
-            data=json_portfolios,
+            data=json_result,
             status=status.HTTP_200_OK,
             )
 
