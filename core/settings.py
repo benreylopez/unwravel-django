e998414452e91dceceecde2a2c44e1f3df1e7e9d
Super Dev <superdev88@hotmail.com>
2019-07-08 13:41:42 +0000

    7.8

--- core/settings.py
+++ core/settings.py
@@ -79,23 +79,11 @@ ROOT_URLCONF = 'core.urls'
 # -----------------------------------------------------------------------------
 # Application definition
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp-mail.outlook.com'
-# EMAIL_PORT = 587
-
-# EMAIL_HOST_USER = 'superdev88@hotmail.com'
-# EMAIL_HOST_PASSWORD = 'spqkfrkfkr4'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-
-# EMAIL_HOST_USER = 'unwravel.app@gmail.com'
-# EMAIL_HOST_PASSWORD = 'Lingerie2019'
-
-
-EMAIL_HOST = 'smtp-mail.outlook.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
+EMAIL_HOST_USER = 'unwravel.app@gmail.com'
+EMAIL_HOST_PASSWORD = 'Lingerie2019'
 
-EMAIL_HOST_USER = 'Rai0826@hotmail.com'
-EMAIL_HOST_PASSWORD = '`1234567890-='
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER #'nysochp@gmail.com' #'Bullxchange <support@mail.bullxchange.io>'
@@ -152,7 +140,7 @@ else:
         'default': {
             'STORAGE_ENGINE': 'MyISAM', # used for south InnoDB would recommended
             'ENGINE': 'django.db.backends.mysql',     # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': 'unwravel_db',               # Or path to database file if using sqlite3.
+            'NAME': 'unwravel_db_new',               # Or path to database file if using sqlite3.
             'USER': 'root',               # Not used with sqlite3.
             'PASSWORD': 'new-password',                # Not used with sqlite3.
             'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
